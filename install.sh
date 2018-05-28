d5505a120d452b117a088f781056fd7cd606c1ab
Martin Shetty <martukas@gmail.com>
2018-05-29 01:22:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 read -r -p "Install and setup conan? [Y/n]" getconan
 getconan=${getconan,,} # tolower
 if [[ $getconan =~ ^(yes|y| ) ]]; then
-sudo apt install pip2
-pip2 install conan
+sudo apt install python-pip
+pip install conan
 conan remote add conancommunity https://api.bintray.com/conan/conan-community/conan
 conan remote add conan-transit https://api.bintray.com/conan/conan/conan-transit
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
