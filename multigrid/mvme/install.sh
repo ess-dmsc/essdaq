2cc732ce84b31b776e7979480ea2bf7886265467
Multigrid NB2 <martin.shetty@esss.se>
2018-07-19 17:39:44 +0200

    mvme installation, config and scripts

---
+++ multigrid/mvme/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mvme_version="0.9.4.1"
+
+#ensure that we are in the script directory
+pushd $(dirname "${BASH_SOURCE[0]}")
+
+sudo arp -s 10.0.0.5 00:00:56:15:30:2b
+
+mvme_file="mvme-$mvme_version-Linux-x64"
+curl -LO http://www.mesytec.com/downloads/mvme/$mvme_file.tar.bz2
+bzip2 -d ./$mvme_file.tar.bz2
+tar xvf ./$mvme_file.tar
+rm -f ./$mvme_file.tar
+rm ./$mvme_file/libz.so.1
+
+yes | cp -rf ./config/* $HOME/
+yes | cp -rf ./config/.config/* $HOME/.config/
