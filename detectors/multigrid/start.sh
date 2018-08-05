fc903acd83db6b07b0e36177de67f10e865ed58d
Martin Shetty <martukas@gmail.com>
2018-08-05 16:49:30 +0200

    modified multigrid config

--- detectors/multigrid/start.sh
+++ detectors/multigrid/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 #ensure that we are in the script directory
-pushd $(dirname "${BASH_SOURCE[0]}")
+pushd $THISDIR
 
 #get config variables
 . ../config/system.sh
 
-../../efu/efu_dump_start.sh
+../../efu/efu_dump_start.sh -f $THISDIR/cfg1.json
 sleep 3
 mvme/scripts/start_mvme.sh $MVME_IP
