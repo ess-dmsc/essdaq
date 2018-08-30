ca956a6a26e8b964d4bf5ff9fb77dd6e9a72285d
MGS @ Multigrid RM2 <martin.shetty@esss.se>
2018-08-30 17:48:27 -0400

    added restart script

---
+++ detectors/multigrid/restart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#ensure that we are in the script directory
+pushd $THISDIR
+
+./stop.sh
+sleep 1
+./dump_start.sh
