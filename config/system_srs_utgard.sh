cdd7cc595a6f2c65986de6f22d4640bc0beac81c
MGS @ mg-p-rm1 <martin.shetty@esss.se>
2018-12-03 11:31:42 +0100

    added srs utgard config

---
+++ config/system_srs_utgard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+EFU_IP=10.0.19.244
+KAFKA_IP=10.0.19.240
+GRAFANA_IP=10.0.19.244
+DAQUIRI_IP=10.0.19.240
+
+EFU_UDP=6006
+UDP_ETH=enp4s0
+
+DETECTOR=gdgem
+
+DUMP_PATH=/home/mg/data/efu_dump
