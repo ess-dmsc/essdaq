eaa67979c187dd2646a4b2c781e443fe5a1b720b
Morten Jagd Christensen <morten.christensen@esss.se>
2019-02-15 15:13:50 +0100

    updated localconfig file

--- detectors/sonde/localconfig.sh
+++ detectors/sonde/localconfig.sh
@@ -5,7 +5,9 @@ export DETECTOR=sonde
 echo "SoNDe - applying local configuration"
 
 # Start EFU in foreground
-#DEBUG="yes"
+#DEBUG=yes
+# can also be run as
+# DEBUG=yes ./start.sh
 
 # Currently used config
 #EFU_CONFIG=<cfgfile_for_sonde>
