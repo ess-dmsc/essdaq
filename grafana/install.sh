7d07798b33f981ab054b170f5dfc565da41186c4
Morten Jagd Christensen <morten.christensen@esss.se>
2019-03-15 14:13:12 +0100

    lsb_release command does not exist for some reason, testing the grep and sed way

--- grafana/install.sh
+++ grafana/install.sh
@@ -5,9 +5,11 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 #ensure that we are in the script directory
 pushd $THISDIR
 
+RELEASE=$(grep CODENAME /etc/lsb-release | sed -e's/^.*=//g')
+
 sudo apt-get install -y curl
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $RELEASE stable"
 sudo apt-get update && sudo apt-get install -y docker-ce
 
 sudo docker swarm init || exit 1
