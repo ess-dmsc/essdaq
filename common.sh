1f0497c326ea03b0e88041232b4810098bbca7c6
Morten Jagd Christensen <morten.christensen@esss.se>
2019-03-18 16:38:52 +0100

    move detectos to common

--- common.sh
+++ common.sh
@@ -3,6 +3,19 @@
 function errexit()
 {
   echo Error: $1
+  exit 1
+}
+
+function detectos()
+{
+  echo "Detect OS and select install script"
+  command -v grep  &>/dev/null || errexit "grep command does not exist"
+  command -v cat   &>/dev/null || errexit "cat command does not exist"
+  command -v uname &>/dev/null || errexit "uname command does not exist"
+
+  cat /etc/centos-release 2>/dev/null | grep CentOS &>/dev/null && export SYSTEM=centos
+  cat /etc/lsb-release 2>/dev/null    | grep Ubuntu &>/dev/null && export SYSTEM=ubuntu
+  uname -a | grep Darwin &>/dev/null && export SYSTEM=macos
 }
 
 function installpkg()
@@ -22,3 +35,5 @@ function installpkg()
     yum install -y $CENTOSPKG
   fi
 }
+
+detectos
