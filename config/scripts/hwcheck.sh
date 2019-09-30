8b6ea5c469c46cab5ba367530bae6af4d2ca5487
Morten Jagd Christensen <morten.christensen@esss.se>
2019-09-30 13:18:43 +0200

    make additional check for kernel parameter max_backlog - differs between CentOS and Ubuntu

--- config/scripts/hwcheck.sh
+++ config/scripts/hwcheck.sh
@@ -5,6 +5,21 @@ MYOS=$(uname)
 rmemsize=12582912
 backlogsize=5000
 
+BACKLOG=""
+if [[ -f /etc/centos-release ]]; then
+  BACKLOG=net.core.netdev_max_backlog
+  echo "CentOS: "$BACKLOG
+fi
+
+if [[ -f /etc/lsb-release ]]; then
+  BACKLOG=net.core.netdev.max_backlog
+  echo "Ubuntu: "$BACKLOG
+fi
+
+if [[ $BACKLOG == "" ]]; then
+  errexit "Unsupported Linux distro"
+fi
+
 #
 ###
 #
@@ -14,7 +29,7 @@ function setbuffersizes()
   echo "Setting buffersizes temporary. Consider doing this permanently"
   sudo sysctl -w net.core.rmem_max=$rmemsize
   sudo sysctl -w net.core.wmem_max=$rmemsize
-  sudo sysctl -w net.core.netdev.max_backlog=$backlogsize
+  sudo sysctl -w $BACKLOG=$backlogsize
 }
 
 #
@@ -32,11 +47,12 @@ ifconfig $ETHIF | grep $ETHIF | grep "mtu 9000" &>/dev/null || errexit "ethif [$
 
 if [[ $MYOS != "Darwin" ]] ;
 then
+  echo "Checking kernel buffer sizes"
   sysctl -a 2>/dev/null | grep net.core.rmem_max | grep $rmemsize || setbuffersizes
 
   sysctl -a 2>/dev/null | grep net.core.rmem_max | grep $rmemsize || errexit "rmem_max size incorrect"
   sysctl -a 2>/dev/null | grep net.core.wmem_max | grep $rmemsize || errexit "wmem_max size incorrect"
-  sysctl -a 2>/dev/null | grep net.core.netdev.max_backlog | grep $backlogsize || errexit "max_backlog size incorrect"
+  sysctl -a 2>/dev/null | grep $BACKLOG | grep $backlogsize || errexit "max_backlog size incorrect"
 else
   echo "Skipping receive buffer check for MacOS!!"
 fi
