ffa158e5902c55ef2fbef61f5a7638bf40c03a8d
Afonso Mukai <28659574+amues@users.noreply.github.com>
2021-03-15 13:43:08 +0100

    Fix build issues (#36)

--- daquiri/install.sh
+++ daquiri/install.sh
@@ -6,7 +6,7 @@ pushd $(dirname "${BASH_SOURCE[0]}")
 . ../config/scripts/numcpus.sh
 . ../config/scripts/sshconfig.sh
 
-sudo apt-get install -y cmake qt5-default || exit 1
+sudo apt-get install -y cmake qt5-default autoconf xutils-dev libtool || exit 1
 if [[ $usessh =~ ^(yes|y| ) ]]; then
   git clone git@github.com:ess-dmsc/daquiri.git || exit 1
 else
