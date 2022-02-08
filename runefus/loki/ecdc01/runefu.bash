8a234da30aeb566922d843d9ca7cd95b060f4952
Morten Jagd Christensen <mortenjc@jcaps.com>
2022-02-08 09:24:31 +0100

    new dir structure

---
+++ runefus/loki/ecdc01/runefu.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+basedir=${EFUBASE:-/home/essdaq/essproj/event-formation-unit}
+module=$basedir/build/modules/loki.so
+config=$basedir/src/modules/loki/configs/STFCTestIII.json
+efu=$basedir/build/bin/efu
+kafka="127.0.0.1:9092"
+grafana="172.30.242.21"
+dataport=9000
+cmdport=8888
+region=9
+
+. ../checkandrun $@
