0c9818612f0e15c8a755415ec5701e9c94154a33
JennyLouise <jenny.walker@live.co.uk>
2023-04-19 17:08:37 +0200

    updated timepix lua script

--- wireshark/lua/timepix.lua
+++ wireshark/lua/timepix.lua
@@ -39,21 +39,33 @@ function timepix_proto.dissector(buffer, pinfo, tree)
 
   while (bytesleft >= datasize)
   do
-    -- Readout Data (DREAM specific)
-    source = buffer(offset    , 2):le_uint()
-    unkn1  = buffer(offset + 2, 2):le_uint()
-    time2  = buffer(offset + 4, 4):le_uint()
-    type = bit.rshift(time2,28)
-    time2 = bit.band(time2,0x0fffffff)
+    -- Readout Data (Timepix specific)
+   
+   -- reading 3 overlapping chunks of data, le_uint doesn't support 64bit ints
+   -- and 'data' field of timepix readout spans first 4 and last 4 bytes
+   local readout_bytes_1 = buffer(offset+4, 4):le_uint()
+   local readout_bytes_2 = buffer(offset+2, 4):le_uint()
+   local readout_bytes_3 = buffer(offset, 4):le_uint()
+   
+   --- applying mask and shifts for each element of readout
+   local readout_type = bit.rshift(bit.band(readout_bytes_1, 0xF0000000), 28)
+   local dcol =         bit.rshift(bit.band(readout_bytes_1, 0x0FE00000), 21) 
+   local spix =         bit.rshift(bit.band(readout_bytes_1, 0x001F8000), 15)
+   local pix =          bit.rshift(bit.band(readout_bytes_1, 0x00007000), 12) 
+   local data =         bit.band(readout_bytes_2, 0x0FFFFFFF)
+   local spidr_time =   bit.band(readout_bytes_3, 0x0000FFFF)
+   
 
-    dtree = timepixhdr:add(buffer(offset, 8),string.format("source %5d, type %d, time 0x%08x, unkn 0x%04x",
-               source, type, time2, unkn1))
+    dtree = timepixhdr:add(buffer(offset, 8),string.format("type %d, dcol %d, spix %d, pix %d, data %d, spidr_time %d",
+    readout_type, dcol, spix, pix, data, spidr_time))
 
     bytesleft = bytesleft - datasize
     offset = offset + datasize
   end
+
 end
 
 -- Register the protocol
 udp_table = DissectorTable.get("udp.port")
-udp_table:add(8192, timepix_proto)
+added = udp_table:add(9888, timepix_proto)
+print(string.format("added: %s\n", tostring(added)))
