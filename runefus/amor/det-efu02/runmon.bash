05e47410ac0453d3b61d292e55d25c8a5560f272
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-09-07 10:45:29 +0200

    added scripts for AMOR@PSI

---
+++ runefus/amor/det-efu02/runmon.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+basedir=${EFUBASE:-/home/essdaq/essproj/event-formation-unit}
+config=$basedir/src/modules/ttlmon/config/freiamon.json
+efu=$basedir/build/bin/ttlmon
+kafka="127.0.0.1:9092"
+grafana="127.0.0.1"
+dataport=9010
+cmdport=8889
+region=9
+
+. ../../checkandrun $@
