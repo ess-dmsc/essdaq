838731fed8435ea4145f91f03ccef391398733d2
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-09-12 15:19:07 +0200

    adding support for kafkacfg (AMOR test)

--- runefus/amor/det-efu02/runefu.bash
+++ runefus/amor/det-efu02/runefu.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 basedir=${EFUBASE:-/home/essdaq/essproj/event-formation-unit}
-config=$basedir/src/modules/freia/configs/amor.json
+config=$PWD/config/amor.json
 efu=$basedir/build/bin/freia
 kafka="127.0.0.1:9092"
+kafkacfg=$PWD/config/kafkacfg.json
 grafana="127.0.0.1"
 dataport=9000
 cmdport=8888
