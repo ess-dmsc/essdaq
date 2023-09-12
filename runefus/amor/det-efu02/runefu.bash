e36a164e824e993182ab8af1958d90e334a32f4f
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-09-12 15:34:41 +0200

    structure kafka brokers a little

--- runefus/amor/det-efu02/runefu.bash
+++ runefus/amor/det-efu02/runefu.bash
@@ -3,8 +3,13 @@
 basedir=${EFUBASE:-/home/essdaq/essproj/event-formation-unit}
 config=$PWD/config/amor.json
 efu=$basedir/build/bin/freia
-kafka="127.0.0.1:9092"
+
+local_kafka="127.0.0.1:9092"
+psi_kafka="127.0.0.1:9092"
+staging_kafka="10.102.80.32:8093"
+kafka=staging_kafka
 kafkacfg=$PWD/config/kafkacfg.json
+
 grafana="127.0.0.1"
 dataport=9000
 cmdport=8888
--- runefus/amor/det-efu02/runmon.bash
+++ runefus/amor/det-efu02/runmon.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 basedir=${EFUBASE:-/home/essdaq/essproj/event-formation-unit}
-config=$basedir/src/modules/ttlmonitor/configs/freiamon.json
+config=$PWD/config/freiamon.json
 efu=$basedir/build/bin/ttlmonitor
-kafka="127.0.0.1:9092"
+
+local_kafka="127.0.0.1:9092"
+psi_kafka="127.0.0.1:9092"
+staging_kafka="10.102.80.32:8093"
+kafka=staging_kafka
+kafkacfg=$PWD/config/kafkacfg.json
+
 grafana="127.0.0.1"
 dataport=9010
 cmdport=8889
