251b8dea09922ca033fd4c4be3929c7b897e6890
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-09-17 16:32:17 +0200

    adding nmx files for budapest

--- runefus/nmx/det-efu-devel01/runefu.bash
+++ runefus/nmx/det-efu-devel01/runefu.bash
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-essdaqdir=${DAQDIR:-/home/essdaq/essproj/essdaq}
 basedir=${EFUBASE:-/home/essdaq/essproj/event-formation-unit}
-config=$basedir/src/modules/nmx/configs/nmx.json
+config=$PWD/config/nmx.json
 efu=$basedir/build/bin/nmx
-kafka="127.0.0.1:9092"
+
+local_kafka="127.0.0.1:9092"
+
+
+kafka=$local_kafka
+kafkacfg=""
+
 grafana="127.0.0.1"
 dataport=9000
 cmdport=8888
 region=9
 
-. $essdaqdir/runefus/checkandrun $@
+. ../../checkandrun $@
