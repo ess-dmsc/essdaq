6c8375349dd5eff5760a8c7fbcea41a6dd2de4bb
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-09-18 16:11:27 +0200

    remove staging kafka

--- runefus/amor/det-efu02/runmon.bash
+++ runefus/amor/det-efu02/runmon.bash
@@ -6,8 +6,7 @@ efu=$basedir/build/bin/ttlmonitor
 
 local_kafka="127.0.0.1:9092"
 psi_kafka="127.0.0.1:9092"
-staging_kafka="10.102.80.32:8093"
-kafka=staging_kafka
+kafka=$local_kafka
 kafkacfg=$PWD/config/kafkacfg.json
 
 grafana="127.0.0.1"
